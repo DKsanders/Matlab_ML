cbc868e5fe02692b5cd397a595b55ef6f85fa458
David Sanders <david.sanders@mail.utoronto.ca>
2016-01-12 21:39:15 -0500

    Implemented handling of temporary variables to speed up learning

--- utils/learning_algos/neural_networks/layered_network/Layer.m
+++ utils/learning_algos/neural_networks/layered_network/Layer.m
@@ -4,16 +4,34 @@ classdef Layer < handle
 
     properties
         activation_function;        % Activation function of layer
+
+        input_holder;               % Memory used to hold inputs and save computation
+        input_size;                 % Size of input_holder
     end
 
     methods
 
-        %
+        % Constructor of Lyaer class
+        function [obj] = Layer()
+            obj.input_size = [0, 0];
+        end
+
+        % Resize temporary matrices to speed up iterations
+        function [] = set_memory_matrix_sizes(obj, num_cases, num_inputs)
+            % Resize matrix for holding inputs
+            if ~( num_cases == obj.input_size(1) && num_inputs == obj.input_size(2))
+                obj.input_holder = ones(num_cases, num_inputs);
+                obj.input_size = [num_cases, num_inputs];
+            end
+        end
+
+        % Activate layer
         function [outputs] = activation(obj, x_inputs, weights)
-            outputs = obj.activation_function.activation(x_inputs, weights);
+            obj.input_holder(:, 2:obj.input_size(2)) = x_inputs;
+            outputs = obj.activation_function.activation(obj.input_holder, weights);
         end
         
-        %
+        % Back propagate from latter layer
         function [weights] = back_propagate(obj, outputs)
             %a;
         end
--- utils/learning_algos/neural_networks/layered_network/LayeredNetwork.m
+++ utils/learning_algos/neural_networks/layered_network/LayeredNetwork.m
@@ -16,43 +16,62 @@ classdef LayeredNetwork < handle
     end
 
     methods
-        %
+
+        % Constructor for layered network
+        % Creates layers and initializes weights
         function [obj] = LayeredNetwork(layer_structure)
             % Get number of layers
             [temp1, temp2] = size(layer_structure);
             obj.num_layers = max(temp1, temp2) - 1;
 
+            % Create hidden layers
             for i=1:obj.num_layers
                 % Create layers
-                obj.layers{i} = Layer;
+                obj.layers{i} = Layer();
                 
                 % Initialize weights
-                obj.weights{i} = ones(layer_structure(i)+1, layer_structure(i+1));
-                
-                obj.layers{i}.activation_function = SigmoidActivation;                
+                obj.weights{i} = ones(layer_structure(i)+1, layer_structure(i+1));  
+
+                obj.layers{i}.activation_function = SigmoidActivation;          
             end
 
+            % Create output layer
+            %obj.layers{obj.num_layers} = OutputLayer;
+            %obj.weights{obj.num_layers} = ones(layer_structure(obj.num_layers)+1, layer_structure(obj.num_layers+1));
+
         end
 
-        %
-        function [weights] = learn(obj, x_inputs)
-            %a;
+        % Learn the weights with training input
+        function [weights] = learn(obj, inputs)
+            % Initialization to speed up computations
+            obj.set_memory_matrix_sizes(inputs);
+            
+            % Learn
+            for i = 1:10
+                obj.forward_propagate(inputs);
+                %obj.backword_propagate();
+            end
         end
 
-        %
+        % Resize temporary matrices to speed up iterations
+        function [outputs] = set_memory_matrix_sizes(obj, x_inputs)
+            [num_cases, num_features] = size(x_inputs);
+            for i=1:obj.num_layers
+                [num_inputs, num_outputs] = size(obj.weights{i});
+                obj.layers{i}.set_memory_matrix_sizes(num_cases, num_inputs);
+            end
+        end
+
+        % Forward propagate through the layers
         function [outputs] = forward_propagate(obj, x_inputs)
             outputs = x_inputs;
             for i=1:obj.num_layers
-                % Extend bias unit
-                [r, c] = size(outputs);
-                outputs = [ones(r,1), outputs];
-
                 % Activate a layer
                 outputs = obj.layers{i}.activation(outputs, obj.weights{i});
             end
         end
         
-        %
+        % Back propagate through the layers
         function [weights] = back_propagate(obj, outputs)
             %a;
         end
