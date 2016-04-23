5aaebab386182e007b0182cafc17ae8c7e4a8b5d
David Sanders <david.sanders@mail.utoronto.ca>
2016-04-22 23:59:16 -0400

    Clarifying whether predictions are probabilities or labels for some descriptions

--- lib/cost_functions/CrossEntropyCostFunction.m
+++ lib/cost_functions/CrossEntropyCostFunction.m
@@ -4,6 +4,7 @@ classdef CrossEntropyCostFunction < CostFunction
 
     methods
         % cost = Σ y*log( P(y) ) + (1-y)*log( 1-P(y) ) / N
+        % y_predictions is a matrix of probabilities
         function [cost] = cost(obj, y_outputs, y_predictions)
             [num_cases, num_outputs] = size(y_outputs);
             cost = 2/(num_cases)*sum(sum(-1*( y_outputs.*log(y_predictions) + (1-y_outputs).*log(1-y_predictions))));
@@ -11,6 +12,7 @@ classdef CrossEntropyCostFunction < CostFunction
 
         % derivative = Σ ( P(y) - y ) * x / N
         % error = Σ 2( P(y) - y )
+        % y_predictions is a matrix of probabilities
         function [output_error] = calculate_error(obj, y_outputs, y_predictions)
             output_error = zeros(size(y_outputs));
             output_error = (y_predictions - y_outputs);
--- lib/evaluation/EvaluateBinaryClassification.m
+++ lib/evaluation/EvaluateBinaryClassification.m
@@ -10,27 +10,27 @@ classdef EvaluateBinaryClassification < handle
         end
 
         % Find precision of predictions
-        function [precision] = get_precision(obj, predictions, y_output)
-            tp = obj.get_true_positives(predictions, y_output);
-            precision = tp ./ sum(predictions);
+        function [precision] = get_precision(obj, label_predictions, y_output)
+            tp = obj.get_true_positives(label_predictions, y_output);
+            precision = tp ./ sum(label_predictions);
         end
 
         % Find recall of predictions
-        function [recall] = get_recall(obj, predictions, y_output)
-            tp = obj.get_true_positives(predictions, y_output);
+        function [recall] = get_recall(obj, label_predictions, y_output)
+            tp = obj.get_true_positives(label_predictions, y_output);
             recall = tp ./ sum(y_output);
         end
 
         % Get F1 score
-        function [score] = get_f1_score(obj, predictions, y_output)
-            precision = obj.get_precision(predictions, y_output);
-            recall = obj.get_recall(predictions, y_output);
+        function [score] = get_f1_score(obj, label_predictions, y_output)
+            precision = obj.get_precision(label_predictions, y_output);
+            recall = obj.get_recall(label_predictions, y_output);
             score = 2 * precision .* recall ./ (precision + recall);
         end
 
         % Get the number of true positives from the predictions
-        function [tp] = get_true_positives(obj, predictions, y_output)
-            tp = sum(and(predictions, y_output));
+        function [tp] = get_true_positives(obj, label_predictions, y_output)
+            tp = sum(and(label_predictions, y_output));
         end
     end
 end
